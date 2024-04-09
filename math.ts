73d72aba913eb46a1d43f25e568eee47a494ca7d
xeron56 <shahidul1425@cseku.ac.bd>
2024-04-09 10:50:21 +0600

    Add new TypeScript files and functions for greetings and math operations

---
+++ math.ts
@@ -0,0 +1,16 @@
+// math.ts
+export function add(a: number, b: number): number {
+	return a + b;
+  }
+  
+  export function subtract(a: number, b: number): number {
+	return a - b;
+  }
+  
+  export function multiply(a: number, b: number): number {
+	return a * b;
+  }
+  
+  export function divide(a: number, b: number): number {
+	return a / b;
+  }
